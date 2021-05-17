4a725519cfff9792b5a8364f77f8c52e05542cee
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-05-17 22:57:00 +0400

    Update index.cpp

--- cpp/index.cpp
+++ cpp/index.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 using namespace std;
 
-long long i, n, j, num[1000001];
 int main(int argc, char** argv) {
-    cin >> n;
-    for(i = 2; i <= n; i++) {
+    long long n; cin >> n;
+    int num[n];
+    for(int i = 2; i <= n; i++) {
         if(num[i] == 1) {
             continue;
         } else {
             cout << i << " ";
-            for(j = i * i; j <= n; j=j+i) {
+            for(int j = i * i; j <= n; j=j+i) {
                 num[j]=1;
             }
         }
