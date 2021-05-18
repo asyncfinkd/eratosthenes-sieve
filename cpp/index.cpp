ed92a756e474be5c96c32676a42fb59d2203cc90
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-05-18 19:35:18 +0400

    Update index.cpp

--- cpp/index.cpp
+++ cpp/index.cpp
@@ -1,43 +1,51 @@
 #include <iostream>
 using namespace std;
-class EratSaceri {
-	private: 
+
+class Sieve {
+
+	private:
 		long long n = 1;
 		int Number;
-	public: 
-		void setNumber (long long n = 1) {
-			this->n = n;
+	
+	public:
+		void setNumber(long long n=1) {
+			this->n=n;
 		}
-		void inputNumber (int n = 1) {
-			cout<<"sheiyvanet raodenoba: "; cin >> n;
-			this->Number = n;
+		
+		void inputNumber(int n=1) {
+			cout << "Sheiyvanet Ricxvi: "; cin>>n;
+			this->Number=n;
 		}
-		void printNumber () {
-			cout<<"raodenoba: "<<Number<<endl;
+		
+		void printNumber() {
+			cout << "raodenoba: "<<Number<<endl;
 		}
+		
 		int getNumber() {
 			return this->Number;
 		}
-		void printArray() {
-		    int newArray[getNumber()];
-		    for(int i = 2; i <= getNumber(); i++) {
-		        if(newArray[i] == 1) {
-		            continue;
-		        } else {
-		            cout << i << " ";
-		            for(int j = i * i; j <= getNumber(); j=j+i) {
-		                newArray[j]=1;
-		            }
-		        }
-		    }
-		}
 		
-};
-int main(int argc, char** argv) {
-	EratSaceri ES1;
-	ES1.inputNumber();
-	ES1.printNumber();
-	ES1.printArray();
+		void printArray() {
+			int newArray[getNumber()];
+			for(int i=2;i<=getNumber();i++) {
+				if(newArray[i]==1) {
+					continue;
+				} else {
+					cout <<i<<" ";
+					for(int j=i*i;j<=getNumber();j=j+i){
+						newArray[j]=1;
+					}
+				}
+			}
+		}
 	
-    return 0;
+};
+
+int main(int argc, char** argv) {
+	Sieve Erathostene;
+	Erathostene.inputNumber();
+	Erathostene.printNumber();
+	Erathostene.printArray();
+	
+	return 0;
 }
