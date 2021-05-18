602d4c54d2582bd508949a1c8825e4aeef2de9a6
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-05-18 16:32:46 +0400

    Update index.cpp

--- cpp/index.cpp
+++ cpp/index.cpp
@@ -1,19 +1,43 @@
 #include <iostream>
 using namespace std;
-
+class EratSaceri {
+	private: 
+		long long n = 1;
+		int Number;
+	public: 
+		void setNumber (long long n = 1) {
+			this->n = n;
+		}
+		void inputNumber (int n = 1) {
+			cout<<"sheiyvanet raodenoba: "; cin >> n;
+			this->Number = n;
+		}
+		void printNumber () {
+			cout<<"raodenoba: "<<Number<<endl;
+		}
+		int getNumber() {
+			return this->Number;
+		}
+		void printArray() {
+		    int newArray[getNumber()];
+		    for(int i = 2; i <= getNumber(); i++) {
+		        if(newArray[i] == 1) {
+		            continue;
+		        } else {
+		            cout << i << " ";
+		            for(int j = i * i; j <= getNumber(); j=j+i) {
+		                newArray[j]=1;
+		            }
+		        }
+		    }
+		}
+		
+};
 int main(int argc, char** argv) {
-    long long n; cin >> n;
-    int num[n];
-    for(int i = 2; i <= n; i++) {
-        if(num[i] == 1) {
-            continue;
-        } else {
-            cout << i << " ";
-            for(int j = i * i; j <= n; j=j+i) {
-                num[j]=1;
-            }
-        }
-    }
-    
+	EratSaceri ES1;
+	ES1.inputNumber();
+	ES1.printNumber();
+	ES1.printArray();
+	
     return 0;
 }
