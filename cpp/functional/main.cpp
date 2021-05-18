e58c5155cef553784a1c5cd829f0aa4d7d3c1632
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-05-18 20:26:58 +0400

    Update main.cpp

--- cpp/functional/main.cpp
+++ cpp/functional/main.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
 
+int main(int argc, char** argv) {
+    long long n; cin >> n;
+    int num[n];
+    for(int i=2;i<=n;i++){
+        if(num[i]==1){
+            continue;
+        }else{
+            cout<<i<<" ";
+            for(int j=i*i;j<=n;j=j+i){
+                num[j]=1;
+            }
+        }
+    }
+}
